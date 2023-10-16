77637f3d95a007f5e72e52c202f697ab1a13f2b4
Mohammed Seoud <m.seoud42@gmail.com>
2023-10-16 22:04:26 +0300

    cc

---
+++ src/data/MoviesData.tsx
@@ -0,0 +1,8 @@
+export const MoviesData=[
+    {
+        name:'Army Of The Dead ',
+        desc:'After a zombie outbreak in Las Vegas, a group of mercenaries takes the ultimate gamble by venturing into the quarantine zone for the greatest heist ever',
+        titleImage:'1.jpg'
+
+    },
+]
